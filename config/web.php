1cd2307db8241a5a163505a74f52a644dd3ca6c2
lzhbrian <lzhbrian@gmail.com>
2015-08-27 17:06:30 +0800

    登陆功能
    将login独立出来
    
    实现登录功能
    
    在/models/User.php里有写user的各种属性
    直接可以用 Yii::$app->user->identity->username 访问username
    同理，可以直接用Yii::$app->user->identity->email 访问email
    等等。（id，email，username，realname，teamname）

--- config/web.php
+++ config/web.php
@@ -17,6 +17,7 @@ $config = [
         'user' => [
             'identityClass' => 'app\models\User',
             'enableAutoLogin' => true,
+            'loginUrl' => ['/login'],
         ],
         'errorHandler' => [
             'errorAction' => 'site/error',
