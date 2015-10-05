3e8f3b673e945a21c499f811c905f0500d19b9c1
fancz2002 <i@fourram.com>
2015-10-05 12:36:16 +0800

    update db.php&user.php
    完善了User的属性

--- models/User.php
+++ models/User.php
@@ -3,185 +3,70 @@
 namespace app\models;
 
 use Yii;
-use yii\db\ActiveRecord;
 
-class User extends /* \yii\base\Object */ \yii\db\ActiveRecord implements \yii\web\IdentityInterface
+/**
+ * This is the model class for table "User".
+ *
+ * @property integer $id
+ * @property string $username
+ * @property string $password
+ * @property string $email
+ * @property string $realname
+ * @property string $school
+ * @property string $class
+ * @property string $studentnumber
+ * @property string $authKey
+ * @property string $accessToken
+ * @property integer $teamid
+ * @property integer $status
+ * @property string $created_at
+ * @property string $updated_at
+ * @property string $group
+ */
+class User extends \yii\db\ActiveRecord
 {
-    public $id;
-    public $email;
-    public $username;
-    public $password;
-    public $realname;
-    public $teamname;
-    public $authKey;
-    public $accessToken;
-
-/*
-    private static $users = [
-        '100' => [
-            'id' => '100',
-            'username' => 'admin',
-            'password' => 'admin',
-            'authKey' => 'test100key',
-            'accessToken' => '100-token',
-        ],
-        '101' => [
-            'id' => '101',
-            'username' => 'demo',
-            'password' => 'demo',
-            'authKey' => 'test101key',
-            'accessToken' => '101-token',
-        ],
-    ];
-*/
-
-    public static function tableName() 
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
     {
-        return 'user';
+        return 'User';
     }
 
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['id', 'username', 'password', 'email', 'realname', 'school', 'class', 'studentnumber', 'authKey', 'accessToken', 'teamid', 'status', 'created_at', 'updated_at', 'group'], 'required'],
+            [['id', 'studentnumber', 'teamid', 'status'], 'integer'],
+            [['username', 'password', 'email', 'realname', 'school', 'class', 'authKey', 'accessToken', 'group'], 'string'],
+            [['created_at', 'updated_at'], 'safe']
+        ];
+    }
 
+    /**
+     * @inheritdoc
+     */
     public function attributeLabels()
     {
         return [
-            'id' => 'ID',
-            'email' => 'Email',
-            'username' => 'Username',
-            'password' => 'Password',
-            'realname' => 'Real Name',
-            'teamname' => 'Team Name',
-            'authKey' => 'AuthKey',
-            'accessToken' => 'AccessToken',
-
+            'id' => Yii::t('app', 'ID'),
+            'username' => Yii::t('app', 'Username'),
+            'password' => Yii::t('app', 'Password'),
+            'email' => Yii::t('app', 'Email'),
+            'realname' => Yii::t('app', 'Realname'),
+            'school' => Yii::t('app', 'School'),
+            'class' => Yii::t('app', 'Class'),
+            'studentnumber' => Yii::t('app', 'Studentnumber'),
+            'authKey' => Yii::t('app', 'Auth Key'),
+            'accessToken' => Yii::t('app', 'Access Token'),
+            'teamid' => Yii::t('app', 'Teamid'),
+            'status' => Yii::t('app', 'Status'),
+            'created_at' => Yii::t('app', 'Created At'),
+            'updated_at' => Yii::t('app', 'Updated At'),
+            'group' => Yii::t('app', 'Group'),
         ];
-    }
-    
-
-
-    /**
-     * @inheritdoc
-     */
-    public static function findIdentity($id)
-    {
-        /*
-        return isset(self::$users[$id]) ? new static(self::$users[$id]) : null;
-        */
-        //return static::findOne($id);
-        $user = User::find()->where(array('id' => $id))->asArray()->one();
-        if ($user) {
-            return new static($user);
-        }
- 
-        return null;
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public static function findIdentityByAccessToken($token, $type = null)
-    {
-        /*
-        foreach (self::$users as $user) {
-            if ($user['accessToken'] === $token) {
-                return new static($user);
-            }
-        }
-
-        return null;
-        */
-        //return static::findOne(['access_token' => $token]);
-        $user = User::find()->where(array('accessToken' => $token))->asArray()->one();
-        if ($user) {
-            return new static($user);
-        }
-        return null;
-    }
-
-    /**
-     * Finds user by username
-     *
-     * @param  string      $username
-     * @return static|null
-     */
-    public static function findByUsername($username)
-    {
-        /*
-        foreach (self::$users as $user) {
-            if (strcasecmp($user['username'], $username) === 0) {
-                return new static($user);
-            }
-        }
-
-        return null;
-        */
-
-        $user = User::find()->where(array('username' => $username))->asArray()->one();
-        if ($user) {
-            return new static($user);
-        }
- 
-        return null;
-    }
-
-
-    /**
-     * @inheritdoc
-     */
-    public function getId()
-    {
-        return $this->id;
-    }
-
-
-    /**
-     * @inheritdoc
-     */
-    public function getEmail()
-    {
-        return $this->email;
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function getRealname()
-    {
-        return $this->realname;
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function getTeamname()
-    {
-        return $this->teamname;
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function getAuthKey()
-    {
-        return $this->authKey;
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function validateAuthKey($authKey)
-    {
-        return $this->authKey === $authKey;
-    }
-
-    /**
-     * Validates password
-     *
-     * @param  string  $password password to validate
-     * @return boolean if password provided is valid for current user
-     */
-    public function validatePassword($password)
-    {
-        
-        return $this->password === $password;
     }
 }
