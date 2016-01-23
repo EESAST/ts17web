6b9bf0229d1fe1a4d180eb98ce8b5ff11062f291
lzhbrian <lzhbrian@gmail.com>
2016-01-24 01:02:25 +0800

    写了一些选手文件下载界面，文件放在web/files下面

--- controllers/SupportingFilesController.php
+++ controllers/SupportingFilesController.php
@@ -2,10 +2,15 @@
 
 namespace app\controllers;
 
+use Yii;
+
 class SupportingFilesController extends \yii\web\Controller
 {
     public function actionIndex()
     {
+    	if (Yii::$app->user->isGuest) {
+            return $this->render('/site/index');
+        }//如果没登陆就进不了下载页面
         return $this->render('index');
     }
 
