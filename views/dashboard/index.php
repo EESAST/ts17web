69a9a370735cc6ee4d19c6e1d9d36e2cadf22b59
lzhbrian <lzhbrian@gmail.com>
2016-01-28 15:09:15 +0800

    给user加了密码的hash，authkey的生成，改进了registerform，之前没有在服务器端验证；然后写了一点点dashboard

--- views/dashboard/index.php
+++ views/dashboard/index.php
@@ -2,9 +2,12 @@
 /* @var $this yii\web\View */
 $this->title = 'Dashboard';
 ?>
-<h1>dashboard/index</h1>
+<h1 align="center">欢迎你，<?=Yii::$app->user->identity->realname?></h1>
 
-<p>
-    You may change the content of this page by modifying
-    the file <code><?= __FILE__; ?></code>.
-</p>
+<p align="center">距离队式程序设计大赛提交初赛代码还有x天</p>
+
+<p align="center">距离队式程序设计大赛决赛，走向人生巅峰还有x天</p>
+
+<p>修改个人资料(加照片)</p>
+<p>查看队伍信息</p>
+<p>你的队伍当前上传的代码为</p>
