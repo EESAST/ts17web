fbd93e3a89036c2d0a7d07cf24b9a4d2bb8005f7
lzhbrian <lzhbrian@gmail.com>
2016-02-02 18:20:34 +0800

    论坛写到一半。。存一下

--- views/site/error.php
+++ views/site/error.php
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 <?php
 
 /* @var $this yii\web\View */
@@ -35,32 +35,4 @@ $this->title = $name;?>
   <!-- content end -->
 
 </div>
-=======
-<?php
 
-/* @var $this yii\web\View */
-/* @var $name string */
-/* @var $message string */
-/* @var $exception Exception */
-
-use yii\helpers\Html;
-
-$this->title = $name;
-?>
-<div class="site-error">
-
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <div class="alert alert-danger">
-        <?= nl2br(Html::encode($message)) ?>
-    </div>
-
-    <p>
-        The above error occurred while the Web server was processing your request.
-    </p>
-    <p>
-        Please contact us if you think this is a server error. Thank you.
-    </p>
-
-</div>
->>>>>>> lzhbrian
