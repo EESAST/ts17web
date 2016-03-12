767fcf1b64c6b7a364af661170003479fcae3d03
XgDuan <dxg14@mails.tsinghua.edu.cn>
2016-03-12 22:28:16 +0800

    个人资料就这样，我实在搞不定那个神奇的问题

--- views/dashboard/index.php
+++ views/dashboard/index.php
@@ -5,35 +5,57 @@ use yii\helpers\Url;
 ?>
 <br/>
 <div>
-<br/>
-      <div class="am-fl"><strong class="am-text-primary am-text-lg">个人资料</strong> / <small>Personal information</small></div>
-      <br/>
-<div class="am-cf">
+    <br/>
+    <div class="am-fl"><strong class="am-text-primary am-text-lg">个人资料</strong> / <small>Personal information</small></div>
 
-<table>
-
-
-
-
-</table>
-
-    <ul>
-      <li>
-        头像：<img src="touxiang/".<?=$model->username?>.".png" onerror="this.src='images/star4.png'" width="100" height="100" >&nbsp&nbsp
-        <button>更换头像</button>
-
-      </li>
-      <li>用户名：<?=$model->username?>&nbsp&nbsp&nbsp
-          <a href="<?php echo Url::to(['dashboard/verifyoldpwd'])?>">
-            <button>修改密码</button>
-          </a>
-      </li>
-      <li>邮箱：<?=$model->email?></li>
-      <li>真实姓名：<?=$model->realname?></li>
-      <li>学校：<?=$model->school?></li>
-      <li>班级：<?=$model->class?></li>
-      <li>学生证号：<?=$model->studentnumber?></li>
-    </ul>
+    <br/>
 </div>
-  <!-- content end -->
+<br/>
+<div class="am-g">
+    <div class="am-u-sm-8 am-u-lg-8">
+        <div class="am-input-group am-input-group-primary">
+            <span class="am-input-group-label"><i class="am-icon-user am-icon-fw"></i></span>
+            <input type="text" class="am-form-field" disabled="disabled" value="<?="姓名： ".$model->username ?>">
+        </div>
+        <br/>
+        <div class="am-input-group am-input-group-secondary">
+            <span class="am-input-group-label"><i class="am-icon-envelope am-icon-fw"></i></span>
+            <input type="text" class="am-form-field" disabled="disabled" value="<?="邮箱： ".$model->email ?>">
+        </div>
+        <br/>
+        <div class="am-input-group am-input-group-success">
+            <span class="am-input-group-label"><i class="am-icon-user am-icon-fw"></i></span>
+            <input type="text" class="am-form-field" disabled="disabled" value="<?="真实姓名：".$model->realname ?>">
+        </div>
+        <br/>
+        <div class="am-input-group am-input-group-warning">
+            <span class="am-input-group-label"><i class="am-icon-graduation-cap am-icon-fw"></i></span>
+            <input type="text" class="am-form-field" disabled="disabled" value="<?="学校：".$model->school ?>">
+        </div>
+        <br/>
+        <div class="am-input-group am-input-group-danger">
+            <span class="am-input-group-label"><i class="am-icon-users am-icon-fw"></i></span>
+            <input type="text" class="am-form-field" disabled="disabled" value="<?="班级： ".$model->email ?>">
+        </div>
+        <br/>
+        <div class="am-input-group am-input-group-danger">
+            <span class="am-input-group-label"><i class="am-icon-tag am-icon-fw"></i></span>
+            <input type="text" class="am-form-field" disabled="disabled" value="<?="学生证号： ".$model->studentnumber ?>">
+        </div>
+        <br/>
+
+        <a class="am-link-muted" href="<?php echo Url::to(['dashboard/verifyoldpwd'])?>">
+            <button  class="am-btn am-btn-default am-btn-block">修改密码</button>
+        </a>
+    </div>
+    <div class="am-u-sm-4 am-u-lg-4">
+        <div style="width:120px;border: grey 1px solid;padding-top: 10px;">
+        <img class="am-center" src="touxiang/".<?=$model->username?>.".png" onerror="this.src='images/star4.png'" width="100" height="100" >
+        <button class="am-btn am-btn-block" id="button">更换头像</button>
+        </div>
+        <script>
+            $("button").hover(function(){$(this).addClass("am-btn-primary")});
+            $("button").mouseleave(function(){$(this).removeClass("am-btn-primary")});
+        </script>
+    </div>
 </div>
