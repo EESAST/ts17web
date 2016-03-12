773470e9369f68422e6b60a1df365f93cbd50e0e
Lin <Brian@LinTzu-Hengs-MacBook-Air.local>
2016-03-12 19:16:05 +0800

    个人资料修改密码那里一直有个bug我调不好＝ ＝

--- views/dashboard/index.php
+++ views/dashboard/index.php
@@ -1,44 +1,39 @@
 <?php 
 use app\doc\widgets\myActiveForm;
 use yii\helpers\Html;
+use yii\helpers\Url;
 ?>
 <br/>
 <div>
 <br/>
       <div class="am-fl"><strong class="am-text-primary am-text-lg">个人资料</strong> / <small>Personal information</small></div>
       <br/>
-      <hr/><small>本页面处于开发阶段，请随意吐槽</small><hr/>
-      <hr/><small>目前暂不提供信息修改功能，请见谅</small><hr/>
-      <br/>
 <div class="am-cf">
-    <?php $form = myActiveForm::begin([
-        'id' => 'register-form',
-        'options' => ['class' => 'form-horizontal','enctype' => 'multipart/form-data'],
-        'fieldConfig' => [
-            'template' => "{label}{input}",
-            'labelOptions' => ['class' => 'col-lg-1 control-label'],
-            'errorOptions'=>['class'=>'am-text-danger']
-        ],
-    ]); ?>
-        <?= $form->field($model, 'username',['placeholder'=>'请设置一个用户名(应输入6-32个字符，汉字按照1个字符计算)']) ?>
 
-        <?= $form->field($model, 'password',['placeholder'=>'请设置登录密码(至少应包含6个字符)'])->passwordInput() ?>
-        <?= $form->field($model, 'email',['placeholder'=>'请输入您的邮箱(该邮箱将在找回密码)']) ?>
+<table>
 
-        <?= $form->field($model, 'realname',['placeholder'=>'请输入您的真实姓名(这有助于我们确定您的真实身份)']) ?>
 
-        <?= $form->field($model, 'school',['placeholder'=>'请输入您的学校全称(五道口男子技术学院?)']) ?>
 
-        <?= $form->field($model, 'class',['placeholder'=>'请输入您的院系-班级(例:电子工程系-无59班)']) ?>
 
-        <?= $form->field($model, 'studentnumber',['placeholder'=>'请输入您的学号(例:2015012345)']) ?>
+</table>
 
-        
-        <?= Html::submitButton('保存修改', ['class' => 'am-btn am-btn-primary am-btn-block', 'name' => 'register-button']) ?>
-    
-    <?php myActiveForm::end(); ?>
+    <ul>
+      <li>
+        头像：<img src="touxiang/".<?=$model->username?>.".png" onerror="this.src='images/star4.png'" width="100" height="100" >&nbsp&nbsp
+        <button>更换头像</button>
+
+      </li>
+      <li>用户名：<?=$model->username?>&nbsp&nbsp&nbsp
+          <a href="<?php echo Url::to(['dashboard/verifyoldpwd'])?>">
+            <button>修改密码</button>
+          </a>
+      </li>
+      <li>邮箱：<?=$model->email?></li>
+      <li>真实姓名：<?=$model->realname?></li>
+      <li>学校：<?=$model->school?></li>
+      <li>班级：<?=$model->class?></li>
+      <li>学生证号：<?=$model->studentnumber?></li>
+    </ul>
 </div>
   <!-- content end -->
-  <br/>
-  <br/>
-  </div>
+</div>
